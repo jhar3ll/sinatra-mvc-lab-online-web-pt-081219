d211035d082fc8c6770f3a460476c0148043ae68
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-19 01:22:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,13 @@
 class PigLatinizer
 
   def piglatinize(input_str)
-    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    if input_str.split(" ").length == 1 
+      piglatinize_word(input_str) 
+    else 
+      piglatinize_sentence(input_str)
   end
+end 
 
-  private
 
   def consonant?(char)
     !char.match(/[aAeEiIoOuU]/)
