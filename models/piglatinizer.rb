594af7ecb43001cdd20f01c847a308e75d93313e
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-19 00:44:03 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,23 +6,25 @@ class PigLatinizer
     else 
       piglatinize_sentence(input_str)
   end
-
-  private
+  
 
   def consonant?(char)
     !char.match(/[aAeEiIoOuU]/)
   end
 
   def piglatinize_word(word)
-    # word starts with vowel
+  
     if !consonant?(word[0])
-      word = word + "w"
+      word += "w"
+      
     # word starts with 3 consonants
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
+      
     # word starts with 2 consonants
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
+      
     # word starts with 1 consonant
     else
       word = word.slice(1..-1) + word.slice(0)
